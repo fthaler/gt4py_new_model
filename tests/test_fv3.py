299111b8519411addd28d5c556b666795e110d98
Felix Thaler <thaler@cscs.ch>
2021-03-24 16:15:29 +0100

    Added do-what-you-want-with-your-column example

--- tests/test_fv3.py
+++ tests/test_fv3.py
@@ -83,10 +83,13 @@ def ifs1_impl1(qi, qs, qg):
 def ifs1_fun1(qi, qs, qg):
     qsum = qi + qs
     return (
-        (0.0, 0.0, qg + qsum) if qsum <= 0.0 else
-        (0.0, qsum, qg) if qi < 0.0 else
-        (qsum, 0.0, qg) if qs < 0.0 else
-        (qi, qs, qg)
+        (0.0, 0.0, qg + qsum)
+        if qsum <= 0.0
+        else (0.0, qsum, qg)
+        if qi < 0.0
+        else (qsum, 0.0, qg)
+        if qs < 0.0
+        else (qi, qs, qg)
     )
 
 
@@ -99,9 +102,7 @@ def ifs1_fun3(qi, qs, qg):
     qs1, qg1 = ifs1_fun2(qs, qg)
     qi1, qg2 = ifs1_fun2(qi, qg1)
     return (
-        (qi, qs, qg) if qg >= 0.0 else
-        (qi, qs1, qg1) if qg1 >= 0.0 else
-        (qi1, qs1, qg2)
+        (qi, qs, qg) if qg >= 0.0 else (qi, qs1, qg1) if qg1 >= 0.0 else (qi1, qs1, qg2)
     )
 
 
@@ -140,3 +141,59 @@ def test_ifs1():
     assert np.allclose(qi_out, qi_out_s)
     assert np.allclose(qs_out, qs_out_s)
     assert np.allclose(qg_out, qg_out_s)
+
+
+def no_idea_what_it_is_doing_reference():
+    rng = np.random.default_rng()
+    shape = (3, 5, 7)
+    qv_in = rng.normal(size=shape)
+    dp = rng.normal(size=shape)
+    qv = np.copy(qv_in)
+
+    for k in range(1, shape[2] - 1):
+        # https://github.com/GridTools/concepts/issues/22#issuecomment-686555219
+        dq = np.minimum(-qv[:, :, k] * dp[:, :, k], qv[:, :, k - 1] * dp[:, :, k - 1])
+        mask = (qv[:, :, k] < 0) & (qv[:, :, k - 1] > 0.0)
+        qv[:, :, k - 1][mask] -= (dq / dp[:, :, k - 1])[mask]
+        qv[:, :, k][mask] += (dq / dp[:, :, k])[mask]
+        mask = qv[:, :, k] < 0.0
+        qv[:, :, k + 1][mask] += (qv[:, :, k] * dp[:, :, k] / dp[:, :, k + 1])[mask]
+        qv[:, :, k][mask] = 0.0
+
+    return (qv_in, dp), qv
+
+
+def do_what_you_want_with_your_column(func):
+    def wrapper(*args):
+        columns = (np.copy(arg[()]) for arg in args)
+        return func(*columns)
+
+    return wrapper
+
+
+@do_what_you_want_with_your_column
+def no_idea_what_it_is_doing_impl(qv, dp):
+    for k in range(1, qv.size - 1):
+        if qv[k] < 0 and qv[k - 1] > 0.0:
+            dq = min(-qv[k] * dp[k], qv[k - 1] * dp[k - 1])
+            qv[k - 1] -= dq / dp[k - 1]
+            qv[k] += dq / dp[k]
+        if qv[k] < 0.0:
+            qv[k + 1] += qv[k] * dp[k] / dp[k + 1]
+            qv[k] = 0.0
+    return qv
+
+
+def test_no_idea_what_it_is_doing():
+    (qv_in, dp), qv_out = no_idea_what_it_is_doing_reference()
+
+    @fencil
+    def apply(qv_in, dp, qv_out, domain):
+        apply_stencil(no_idea_what_it_is_doing_impl, domain, [qv_out], [qv_in, dp])
+
+    qv_in_s = storage(qv_in)
+    dp_s = storage(dp)
+    qv_out_s = storage(np.zeros_like(qv_out))
+    apply(qv_in_s, dp_s, qv_out_s, domain=domain(qv_in.shape))
+
+    assert np.allclose(qv_out, qv_out_s)
