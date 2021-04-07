c50dc9f5bd5870e8fc625acd430b2337977fb619
Felix Thaler <thaler@cscs.ch>
2021-04-07 13:39:00 +0200

    Added FV3 horizontal advection example with corner fixes

--- tests/test_fv3.py
+++ tests/test_fv3.py
@@ -481,3 +481,205 @@ def test_divergence_corner():
     )
 
     assert np.allclose(divg_d, divg_d_s)
+
+
+def horizontal_advection_reference():
+    shape = (9, 11, 3)
+    rng = np.random.default_rng()
+    u = rng.normal(size=shape)
+    v = rng.normal(size=shape)
+    inp = rng.normal(size=shape)
+
+    inp_xfix = np.copy(inp)
+    inp_xfix[:3, :3, :] = np.rot90(inp_xfix[3:6, :3, :], -1)
+    inp_xfix[:3, -3:, :] = np.rot90(inp_xfix[3:6, -3:, :], 1)
+    inp_xfix[-3:, :3, :] = np.rot90(inp_xfix[-6:-3, :3, :], 1)
+    inp_xfix[-3:, -3:, :] = np.rot90(inp_xfix[-6:-3, -3:, :], -1)
+
+    adv_x = u[3:-3, :, :] * (
+        -inp_xfix[:-6, :, :]
+        + 9 * (inp_xfix[1:-5, :, :] - inp_xfix[5:-1, :, :])
+        - 45 * (inp_xfix[2:-4, :, :] - inp_xfix[4:-2, :, :])
+        + inp_xfix[6:, :, :]
+    ) + np.abs(u[3:-3, :, :]) * (
+        -inp_xfix[:-6, :, :]
+        + 6 * (inp_xfix[1:-5, :, :] + inp_xfix[5:-1, :, :])
+        - 15 * (inp_xfix[2:-4, :, :] + inp_xfix[4:-2, :, :])
+        + 20 * inp_xfix[3:-3, :, :]
+        - inp_xfix[6:, :, :]
+    )
+
+    adv_y = v[3:-3:, 3:-3, :] * (
+        -adv_x[:, :-6, :]
+        + 9 * (adv_x[:, 1:-5, :] - adv_x[:, 5:-1, :])
+        - 45 * (adv_x[:, 2:-4, :] - adv_x[:, 4:-2, :])
+        + adv_x[:, 6:, :]
+    ) + np.abs(v[3:-3, 3:-3, :]) * (
+        -adv_x[:, :-6, :]
+        + 6 * (adv_x[:, 1:-5, :] + adv_x[:, 5:-1, :])
+        - 15 * (adv_x[:, 2:-4, :] + adv_x[:, 4:-2, :])
+        + 20 * adv_x[:, 3:-3, :]
+        - adv_x[:, 6:, :]
+    )
+
+    return (u, v, inp), adv_y
+
+
+def advection_impl(i_start, i_end, j_start, j_end):
+    def advection(dim):
+        @polymorphic_stencil
+        def advect(vel, inp):
+            return vel[()] * (
+                -inp[dim - 3]
+                + 9 * (inp[dim - 2] - inp[dim + 2])
+                - 45 * (inp[dim - 1] - inp[dim + 1])
+                + inp[dim + 3]
+            ) + np.abs(vel[()]) * (
+                -inp[dim - 3]
+                + 6 * (inp[dim - 2] + inp[dim + 2])
+                - 15 * (inp[dim - 1] + inp[dim + 1])
+                + 20 * inp[()]
+                - inp[dim + 3]
+            )
+
+        return advect
+
+    @polymorphic_stencil
+    def corner_fix_lo_i_lo_j(i, j, inp):
+        if j[()] == j_start - 1:
+            if i[()] == i_start - 1:
+                return inp[I + 1]
+            if i[()] == i_start - 2:
+                return inp[I + 2, J - 1]
+            if i[()] == i_start - 3:
+                return inp[I + 3, J - 2]
+        if j[()] == j_start - 2:
+            if i[()] == i_start - 1:
+                return inp[I + 2, J + 1]
+            if i[()] == i_start - 2:
+                return inp[I + 3]
+            if i[()] == i_start - 3:
+                return inp[I + 4, J - 1]
+        if j[()] == j_start - 3:
+            if i[()] == i_start - 1:
+                return inp[I + 3, J + 2]
+            if i[()] == i_start - 2:
+                return inp[I + 4, J + 1]
+            if i[()] == i_start - 3:
+                return inp[I + 5]
+        return inp[()]
+
+    @polymorphic_stencil
+    def corner_fix_lo_i_hi_j(i, j, inp):
+        if j[()] == j_end:
+            if i[()] == i_start - 1:
+                return inp[I + 1]
+            if i[()] == i_start - 2:
+                return inp[I + 2, J + 1]
+            if i[()] == i_start - 3:
+                return inp[I + 3, J + 2]
+        if j[()] == j_end + 1:
+            if i[()] == i_start - 1:
+                return inp[I + 2, J - 1]
+            if i[()] == i_start - 2:
+                return inp[I + 3]
+            if i[()] == i_start - 3:
+                return inp[I + 4, J + 1]
+        if j[()] == j_end + 2:
+            if i[()] == i_start - 1:
+                return inp[I + 3, J - 2]
+            if i[()] == i_start - 2:
+                return inp[I + 4, J - 1]
+            if i[()] == i_start - 3:
+                return inp[I + 5]
+        return inp[()]
+
+    @polymorphic_stencil
+    def corner_fix_hi_i_lo_j(i, j, inp):
+        if j[()] == j_start - 1:
+            if i[()] == i_end:
+                return inp[I - 1]
+            if i[()] == i_end + 1:
+                return inp[I - 2, J - 1]
+            if i[()] == i_end + 2:
+                return inp[I - 3, J - 2]
+        if j[()] == j_start - 2:
+            if i[()] == i_end:
+                return inp[I - 2, J + 1]
+            if i[()] == i_end + 1:
+                return inp[I - 3]
+            if i[()] == i_end + 2:
+                return inp[I - 4, J - 1]
+        if j[()] == j_start - 3:
+            if i[()] == i_end:
+                return inp[I - 3, J + 2]
+            if i[()] == i_end + 1:
+                return inp[I - 4, J + 1]
+            if i[()] == i_end + 2:
+                return inp[I - 5]
+        return inp[()]
+
+    @polymorphic_stencil
+    def corner_fix_hi_i_hi_j(i, j, inp):
+        if j[()] == j_end:
+            if i[()] == i_end:
+                return inp[I - 1]
+            if i[()] == i_end + 1:
+                return inp[I - 2, J + 1]
+            if i[()] == i_end + 2:
+                return inp[I - 3, J + 2]
+        if j[()] == j_end + 1:
+            if i[()] == i_end:
+                return inp[I - 2, J - 1]
+            if i[()] == i_end + 1:
+                return inp[I - 3]
+            if i[()] == i_end + 2:
+                return inp[I - 4, J + 1]
+        if j[()] == j_end + 2:
+            if i[()] == i_end:
+                return inp[I - 3, J - 2]
+            if i[()] == i_end + 1:
+                return inp[I - 4, J - 1]
+            if i[()] == i_end + 2:
+                return inp[I - 5]
+        return inp[()]
+
+    @polymorphic_stencil
+    def corner_fix(i, j, inp):
+        a = lift(corner_fix_lo_i_lo_j)(i, j, inp)
+        b = lift(corner_fix_lo_i_hi_j)(i, j, a)
+        c = lift(corner_fix_hi_i_lo_j)(i, j, b)
+        return corner_fix_hi_i_hi_j(i, j, c)
+
+    @polymorphic_stencil
+    def advection_impl(i, j, u, v, inp):
+        xfix = lift(corner_fix)(i, j, inp)
+        adv_x = lift(advection(I))(u, xfix)
+        return advection(J)(v, adv_x)
+
+    return advection_impl
+
+
+def test_advection():
+    (u, v, inp), out = horizontal_advection_reference()
+    u_s = storage(u, origin=(3, 3, 0))
+    v_s = storage(v, origin=(3, 3, 0))
+    inp_s = storage(inp, origin=(3, 3, 0))
+    out_s = storage(np.zeros_like(out))
+    i_s = index(inp.shape, "i", origin=(3, 3, 0))
+    j_s = index(inp.shape, "j", origin=(3, 3, 0))
+    i_start, i_end = 3, inp.shape[0] - 3
+    j_start, j_end = 3, inp.shape[1] - 3
+
+    @fencil
+    def apply(i, j, u, v, inp, out, domain):
+        apply_stencil(
+            advection_impl(i_start, i_end, j_start, j_end),
+            domain,
+            [out],
+            [i, j, u, v, inp],
+        )
+
+    apply(i_s, j_s, u_s, v_s, inp_s, out_s, domain=domain(out.shape))
+
+    assert np.allclose(out, out_s)
