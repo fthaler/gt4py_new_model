59ca0d4456c98712b901e6d95720d39340ac9cc9
Hannes Vogt <vogt@hey.com>
2021-04-08 16:24:46 +0200

    wip

--- tests/unstructured/test_v2v.py
+++ tests/unstructured/test_v2v.py
@@ -1,5 +1,7 @@
+from dataclasses import dataclass
 import numpy as np
 import math
+import enum
 
 
 def as_1d(arr):
@@ -10,24 +12,40 @@ def as_2d(arr, shape):
     return arr.reshape(*shape)
 
 
-def v2v(acc_in):
-    print(acc_in[0])
-    return acc_in[0] + acc_in[1] + acc_in[2] + acc_in[3]
+def connectivity(*args):
+    def _impl(fun):
+        class conn:
+            # TODO NeighborHood not only in and out location
+            in_location, out_location = args[0], args[1]
+
+            def __call__(self, field):
+                assert hasattr(field, "location")
+                assert field.location == self.in_location
+
+                return fun(field)
+
+        return conn()
+
+    return _impl
 
 
 def make_v2v_conn(shape_2d):
     strides = [shape_2d[1], 1]
 
+    @connectivity(LocationType.Vertex, LocationType.Vertex)
     def v2v_conn(field):
-        def new_field(index):
-            return [
-                field(index + strides[0]),
-                field(index - strides[0]),
-                field(index + strides[1]),
-                field(index - strides[1]),
-            ]
+        class new_field:
+            location = LocationType.Vertex  # TODO information is duplicated
 
-        return new_field
+            def __call__(self, index):
+                return [
+                    field(index + strides[0]),
+                    field(index - strides[0]),
+                    field(index + strides[1]),
+                    field(index - strides[1]),
+                ]
+
+        return new_field()
 
     return v2v_conn
 
@@ -43,27 +61,86 @@ def lift(stencil):
     return lifted
 
 
+@enum.unique
+class LocationType(enum.IntEnum):
+    Vertex = 0
+    Edge = 1
+    Cell = 2
+
+
+# a field is a function from index to element `()` not `[]`
+# (or change the conn)
+def as_field(arr, loc: LocationType):
+    class _field:
+        location = loc
+
+        def __call__(self, i: int):
+            return arr[i]
+
+    return _field()
+
+
+class V2VNeighborHood:
+    left = 0
+    right = 1
+    top = 2
+    bottom = 3
+
+
+vv = V2VNeighborHood()
+
+
+def stencil(*args):
+    def decorator_stencil(fun):
+        return fun
+
+    return decorator_stencil
+
+
+@stencil([vv])
+def v2v(acc_in):
+    return acc_in[vv.left] + acc_in[vv.right] + acc_in[vv.top] + acc_in[vv.bottom]
+
+
+# @stencil([neighborhood, neighborhood])
 def v2v2v(acc_in):
     x = lift(v2v)(acc_in)
     return v2v(x)
 
 
-# a field is a function from index to element `()` not `[]`
-# (or change the conn)
-def as_field(arr):
-    def tmp(i):
-        return arr[i]
+def test_v2v():
+    # TODO define fencil
+    def apply(stencil, domain, v2v_conn, out, inp):
+        for i in domain:
+            out[i] = stencil(v2v_conn(as_field(inp, LocationType.Vertex))(i))
 
-    return tmp
+    shape = (5, 7)
+    # inp = np.random.rand(*shape)
+    inp = np.ones(shape)
+    out1d = np.zeros(math.prod(shape))
+    ref = np.zeros(shape)
+    ref[1:-1, 1:-1] = np.ones((3, 5)) * 4
+
+    inp1d = as_1d(inp)
+
+    domain = np.arange(math.prod(shape))
+    domain_2d = as_2d(domain, shape)
+    inner_domain = as_1d(domain_2d[1:-1, 1:-1])
+
+    v2v_conn = make_v2v_conn(shape)
+
+    print(v2v_conn.in_location)
+    apply(v2v, inner_domain, v2v_conn, out1d, inp1d)
+    out2d = as_2d(out1d, shape)
+    assert np.allclose(out2d, ref)
 
 
-# TODO define fencil
-def apply(stencil, domain, v2v_conn, out, inp):
-    for i in domain:
-        out[i] = stencil(v2v_conn(v2v_conn(as_field(inp)))(i))
+def test_v2v2v():
+    # TODO define fencil
+    def apply(stencil, domain, v2v_conn, out, inp):
+        for i in domain:
+            out[i] = stencil(v2v_conn(v2v_conn(as_field(inp, LocationType.Vertex)))(i))
 
-
-def test_copy():
     shape = (5, 7)
     # inp = np.random.rand(*shape)
     inp = np.ones(shape)
@@ -83,4 +160,5 @@ def test_copy():
     assert np.allclose(out2d, ref)
 
 
-test_copy()
+test_v2v()
+test_v2v2v()
