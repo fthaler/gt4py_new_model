ed1a61653c1589c73ff1e6105231183649134e0c
Hannes Vogt <vogt@hey.com>
2021-04-09 12:59:10 +0200

    wip

--- tests/unstructured/test_v2v.py
+++ tests/unstructured/test_v2v.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 import numpy as np
 import math
+import devtools
 import enum
 
 
@@ -12,11 +13,15 @@ def as_2d(arr, shape):
     return arr.reshape(*shape)
 
 
-def connectivity(*args):
+def connectivity(*_neighborhoods):
     def _impl(fun):
         class conn:
             # TODO NeighborHood not only in and out location
-            in_location, out_location = args[0], args[1]
+            neighborhoods = _neighborhoods
+            in_location, out_location = (
+                neighborhoods[0].in_location,
+                neighborhoods[-1].out_location,
+            )
 
             def __call__(self, field):
                 assert hasattr(field, "location")
@@ -29,25 +34,33 @@ def connectivity(*args):
     return _impl
 
 
-def make_v2v_conn(shape_2d):
-    strides = [shape_2d[1], 1]
+def conn_mult(conn_a, conn_b):
+    assert conn_a.neighborhoods[-1].out_location == conn_b.neighborhoods[0].in_location
 
-    @connectivity(LocationType.Vertex, LocationType.Vertex)
-    def v2v_conn(field):
-        class new_field:
-            location = LocationType.Vertex  # TODO information is duplicated
+    class conn:
+        neighborhoods = [*conn_a.neighborhoods, *conn_b.neighborhoods]
+        in_location, out_location = (
+            neighborhoods[0].in_location,
+            neighborhoods[-1].out_location,
+        )
 
-            def __call__(self, index):
-                return [
-                    field(index + strides[0]),
-                    field(index - strides[0]),
-                    field(index + strides[1]),
-                    field(index - strides[1]),
-                ]
+        def __call__(self, field):
+            return conn_a(conn_b(field))
 
-        return new_field()
+    return conn()
 
-    return v2v_conn
+
+def stencil(*args):
+    def _impl(fun):
+        class decorated_stencil:
+            acc_neighborhood_chains = args
+
+            def __call__(self, *args):
+                return fun(*args)
+
+        return decorated_stencil()
+
+    return _impl
 
 
 def lift(stencil):
@@ -80,6 +93,18 @@ def as_field(arr, loc: LocationType):
     return _field()
 
 
+def neighborhood(in_loc, out_loc):
+    """syntactic sugar to create a neighborhood"""
+
+    def impl(cls):
+        cls.in_location = in_loc
+        cls.out_location = out_loc
+        return cls
+
+    return impl
+
+
+@neighborhood(LocationType.Vertex, LocationType.Vertex)
 class V2VNeighborHood:
     left = 0
     right = 1
@@ -87,17 +112,31 @@ class V2VNeighborHood:
     bottom = 3
 
 
+def make_v2v_conn(shape_2d):
+    strides = [shape_2d[1], 1]
+
+    @connectivity(V2VNeighborHood())
+    def v2v_conn(field):
+        class new_field:
+            location = LocationType.Vertex  # TODO information is duplicated
+
+            def __call__(self, index):
+                return [
+                    field(index + strides[0]),
+                    field(index - strides[0]),
+                    field(index + strides[1]),
+                    field(index - strides[1]),
+                ]
+
+        return new_field()
+
+    return v2v_conn
+
+
 vv = V2VNeighborHood()
 
 
-def stencil(*args):
-    def decorator_stencil(fun):
-        return fun
-
-    return decorator_stencil
-
-
-@stencil([vv])
+@stencil((vv))
 def v2v(acc_in):
     return acc_in[vv.left] + acc_in[vv.right] + acc_in[vv.top] + acc_in[vv.bottom]
 
@@ -108,11 +147,74 @@ def v2v2v(acc_in):
     return v2v(x)
 
 
+def neighborhood_chain_to_key(neighborhood_chain):
+    return tuple(type(nh).__name__ for nh in neighborhood_chain)
+
+
+def apply_stencil(
+    stencil, domain, connectivities, out, inp
+):  # TODO support multiple input/output
+    conn_map = {}
+    for c in connectivities:
+        conn_map[neighborhood_chain_to_key(c.neighborhoods)] = c
+
+    devtools.debug(stencil.acc_neighborhood_chains)
+    stencil_args = conn_map[neighborhood_chain_to_key(stencil.acc_neighborhood_chains)](
+        inp
+    )
+
+    for i in domain:
+        out[i] = stencil(stencil_args(i))
+
+
+@neighborhood(LocationType.Edge, LocationType.Vertex)
+class E2VNeighborHood:
+    pass
+
+
+@neighborhood(LocationType.Vertex, LocationType.Edge)
+class V2ENeighborHood:
+    pass
+
+
+@connectivity(E2VNeighborHood())
+def dummy_e2v_conn(field):
+    class new_field:
+        location = LocationType.Edge
+
+        def __call__(self, index):
+            return []
+
+    return new_field()
+
+
+@connectivity(V2ENeighborHood())
+def dummy_v2e_conn(field):
+    class new_field:
+        location = LocationType.Vertex
+
+        def __call__(self, index):
+            return []
+
+    return new_field()
+
+
+def test_conn_multiply():
+    v2vconn = make_v2v_conn((5, 7))
+    v2v2vconn = conn_mult(v2vconn, v2vconn)
+    print(v2v2vconn.neighborhoods)
+
+    print(conn_mult(dummy_e2v_conn, dummy_v2e_conn).neighborhoods)
+
+
+test_conn_multiply()
+
+
 def test_v2v():
     # TODO define fencil
-    def apply(stencil, domain, v2v_conn, out, inp):
-        for i in domain:
-            out[i] = stencil(v2v_conn(as_field(inp, LocationType.Vertex))(i))
+    # def apply(stencil, domain, v2v_conn, out, inp):
+    #     for i in domain:
+    #         out[i] = stencil(v2v_conn(as_field(inp, LocationType.Vertex))(i))
 
     shape = (5, 7)
     # inp = np.random.rand(*shape)
@@ -130,7 +232,10 @@ def test_v2v():
     v2v_conn = make_v2v_conn(shape)
 
     print(v2v_conn.in_location)
-    apply(v2v, inner_domain, v2v_conn, out1d, inp1d)
+    # apply(v2v, inner_domain, v2v_conn, out1d, inp1d)
+    apply_stencil(
+        v2v, inner_domain, [v2v_conn], out1d, as_field(inp1d, LocationType.Vertex)
+    )
     out2d = as_2d(out1d, shape)
     assert np.allclose(out2d, ref)
 
