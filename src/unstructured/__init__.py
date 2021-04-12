3ebef9e366a2b88726348c3321c65061f3c9daa1
Hannes Vogt <vogt@hey.com>
2021-04-12 17:14:02 +0200

    empty

---
+++ src/unstructured/__init__.py
@@ -0,0 +1,14 @@
+from typing import Optional, Union
+
+from packaging.version import LegacyVersion, Version, parse
+from pkg_resources import DistributionNotFound, get_distribution
+
+
+try:
+    __version__: str = get_distribution("gt4py").version
+except DistributionNotFound:
+    __version__ = "X.X.X.unknown"
+
+__versioninfo__: Optional[Union[LegacyVersion, Version]] = parse(__version__)
+
+del DistributionNotFound, LegacyVersion, Version, get_distribution, parse
