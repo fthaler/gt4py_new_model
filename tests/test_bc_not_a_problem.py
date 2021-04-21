709348af7d62e3524213aac4ec0d4ef693fb284b
Hannes Vogt <vogt@hey.com>
2021-04-21 15:04:58 +0200

    with boundary field

--- tests/test_bc_not_a_problem.py
+++ tests/test_bc_not_a_problem.py
@@ -18,11 +18,11 @@ from gt4py_new_model import *
 
 
 @polymorphic_stencil
-def left_right_sum(inp, index, right_bound):
+def left_right_sum(inp, bc_field, index, right_bound):
     if index[()] == 0:
-        return inp[I + 1]
+        return bc_field[I - 1] + inp[I + 1]
     elif index[()] == right_bound[()] - 1:
-        return inp[I - 1]
+        return inp[I - 1] + bc_field[I + 1]
     else:
         return inp[I - 1] + inp[I + 1]
 
@@ -31,22 +31,24 @@ def test_bc_1level():
     shape = (7, 5, 3)
 
     @fencil
-    def apply(out, inp, domain):
+    def apply(out, inp, bc_field, domain):
         idx = index(shape, "i")
         right_bound = constant(shape[0])
-        apply_stencil(left_right_sum, domain, [out], [inp, idx, right_bound])
+        apply_stencil(left_right_sum, domain, [out], [inp, bc_field, idx, right_bound])
 
     rng = np.random.default_rng()
     inp = storage(rng.normal(size=shape))
-    # inp = storage(np.arange(math.prod(shape)).reshape(shape))
     out = storage(rng.normal(size=shape))
+    bc_field = storage(
+        rng.normal(size=(shape[0] + 2, shape[1], shape[2])), origin=(1, 0, 0)
+    )
 
     ref = np.empty_like(np.asarray(out))
-    ref[-1, :, :] = np.asarray(inp)[-2, :, :]
-    ref[0, :, :] = np.asarray(inp)[1, :, :]
+    ref[-1, :, :] = np.asarray(bc_field)[-1, :, :] + np.asarray(inp)[-2, :, :]
+    ref[0, :, :] = np.asarray(inp)[1, :, :] + np.asarray(bc_field)[0, :, :]
     ref[1:-1, :, :] = np.asarray(inp)[:-2, :, :] + np.asarray(inp)[2:, :, :]
 
-    apply(out, inp, domain=domain(shape[0], shape[1], shape[2]))
+    apply(out, inp, bc_field, domain=domain(shape[0], shape[1], shape[2]))
     assert np.all(ref == np.asarray(out)[:, :, :])
 
 
@@ -57,28 +59,34 @@ def test_bc_2level_fencil():
     shape = (7, 5, 3)
 
     @fencil
-    def apply(out, inp, domain):
+    def apply(out, inp, bc_field, domain):
         idx = index(shape, "i")
         right_bound = constant(shape[0])
-        apply_stencil(left_right_sum, domain, [out], [inp, idx, right_bound])
+        apply_stencil(left_right_sum, domain, [out], [inp, bc_field, idx, right_bound])
 
     rng = np.random.default_rng()
     inp = storage(rng.normal(size=shape))
     # inp = storage(np.arange(math.prod(shape)).reshape(shape))
     out = storage(rng.normal(size=shape))
+    bc_field1 = storage(
+        rng.normal(size=(shape[0] + 2, shape[1], shape[2])), origin=(1, 0, 0)
+    )
+    bc_field2 = storage(
+        rng.normal(size=(shape[0] + 2, shape[1], shape[2])), origin=(1, 0, 0)
+    )
 
     ref = np.empty_like(np.asarray(out))
-    ref[-1, :, :] = np.asarray(inp)[-2, :, :]
-    ref[0, :, :] = np.asarray(inp)[1, :, :]
+    ref[-1, :, :] = np.asarray(bc_field1)[-1, :, :] + np.asarray(inp)[-2, :, :]
+    ref[0, :, :] = np.asarray(inp)[1, :, :] + np.asarray(bc_field1)[0, :, :]
     ref[1:-1, :, :] = np.asarray(inp)[:-2, :, :] + np.asarray(inp)[2:, :, :]
     tmp = ref.copy()
-    ref[-1, :, :] = np.asarray(tmp)[-2, :, :]
-    ref[0, :, :] = np.asarray(tmp)[1, :, :]
+    ref[-1, :, :] = np.asarray(bc_field2)[-1, :, :] + np.asarray(tmp)[-2, :, :]
+    ref[0, :, :] = np.asarray(tmp)[1, :, :] + np.asarray(bc_field2)[0, :, :]
     ref[1:-1, :, :] = np.asarray(tmp)[:-2, :, :] + np.asarray(tmp)[2:, :, :]
 
     tmp_s = storage(rng.normal(size=shape))
-    apply(tmp_s, inp, domain=domain(shape[0], shape[1], shape[2]))
-    apply(out, tmp_s, domain=domain(shape[0], shape[1], shape[2]))
+    apply(tmp_s, inp, bc_field1, domain=domain(shape[0], shape[1], shape[2]))
+    apply(out, tmp_s, bc_field2, domain=domain(shape[0], shape[1], shape[2]))
     assert np.all(ref == np.asarray(out)[:, :, :])
 
 
@@ -86,35 +94,43 @@ test_bc_2level_fencil()
 
 
 @polymorphic_stencil
-def bc_2level_stencil(inp, index, right_bound):
-    level1 = lift(left_right_sum)(inp, index, right_bound)
-    return left_right_sum(level1, index, right_bound)
+def bc_2level_stencil(inp, bc1, bc2, index, right_bound):
+    level1 = lift(left_right_sum)(inp, bc1, index, right_bound)
+    return left_right_sum(level1, bc2, index, right_bound)
 
 
 def test_bc_2level_stencil():
     shape = (7, 5, 3)
 
     @fencil
-    def apply(out, inp, domain):
+    def apply(out, inp, bc1, bc2, domain):
         idx = index(shape, "i")
         right_bound = constant(shape[0])
-        apply_stencil(bc_2level_stencil, domain, [out], [inp, idx, right_bound])
+        apply_stencil(
+            bc_2level_stencil, domain, [out], [inp, bc1, bc2, idx, right_bound]
+        )
 
     rng = np.random.default_rng()
     inp = storage(rng.normal(size=shape))
     # inp = storage(np.arange(math.prod(shape)).reshape(shape))
     out = storage(rng.normal(size=shape))
+    bc_field1 = storage(
+        rng.normal(size=(shape[0] + 2, shape[1], shape[2])), origin=(1, 0, 0)
+    )
+    bc_field2 = storage(
+        rng.normal(size=(shape[0] + 2, shape[1], shape[2])), origin=(1, 0, 0)
+    )
 
     ref = np.empty_like(np.asarray(out))
-    ref[-1, :, :] = np.asarray(inp)[-2, :, :]
-    ref[0, :, :] = np.asarray(inp)[1, :, :]
+    ref[-1, :, :] = np.asarray(bc_field1)[-1, :, :] + np.asarray(inp)[-2, :, :]
+    ref[0, :, :] = np.asarray(inp)[1, :, :] + np.asarray(bc_field1)[0, :, :]
     ref[1:-1, :, :] = np.asarray(inp)[:-2, :, :] + np.asarray(inp)[2:, :, :]
     tmp = ref.copy()
-    ref[-1, :, :] = np.asarray(tmp)[-2, :, :]
-    ref[0, :, :] = np.asarray(tmp)[1, :, :]
+    ref[-1, :, :] = np.asarray(bc_field2)[-1, :, :] + np.asarray(tmp)[-2, :, :]
+    ref[0, :, :] = np.asarray(tmp)[1, :, :] + np.asarray(bc_field2)[0, :, :]
     ref[1:-1, :, :] = np.asarray(tmp)[:-2, :, :] + np.asarray(tmp)[2:, :, :]
 
-    apply(out, inp, domain=domain(shape[0], shape[1], shape[2]))
+    apply(out, inp, bc_field1, bc_field2, domain=domain(shape[0], shape[1], shape[2]))
     assert np.all(ref == np.asarray(out)[:, :, :])
 
 
