cf77cb3db056b357031ddbf544692ba2dbf8b02b
Hannes Vogt <vogt@hey.com>
2021-07-05 09:11:05 +0200

    add dsl

--- src/unstructured/ir2.py
+++ src/unstructured/ir2.py
@@ -9,6 +9,10 @@ from eve.codegen import MakoTemplate as as_mako
 from devtools import debug
 from yasi import indent_code
 from unstructured.sym_validation import validate_symbol_refs
+import inspect
+
+
+_fundefs = []  # something like this or collect only reachable functions
 
 
 class Sym(Node):  # helper
@@ -16,7 +20,17 @@ class Sym(Node):  # helper
 
 
 class Expr(Node):
-    ...
+    def __add__(self, other):
+        return FunCall(name=SymRef(id="plus"), args=[self, other])
+
+    def __radd__(self, other):
+        return FunCall(name=SymRef(id="plus"), args=[make_node(other), self])
+
+    def __sub__(self, other):
+        return FunCall(name=SymRef(id="minus"), args=[self, other])
+
+    def __call__(self, *args):
+        return FunCall(name=self, args=[*args])
 
 
 class IntLiteral(Expr):
@@ -31,12 +45,6 @@ class OffsetLiteral(Expr):
     value: Union[int, str]
 
 
-class FunctionDefinition(Node, SymbolTableTrait):
-    id: SymbolName
-    params: List[Sym]
-    expr: Expr
-
-
 class SymRef(Expr):
     id: SymbolRef
 
@@ -49,6 +57,15 @@ class Lambda(Expr, SymbolTableTrait):
 class FunCall(Expr):
     name: Expr
     args: List[Expr]
+
+
+class FunctionDefinition(Node, SymbolTableTrait):
+    id: SymbolName
+    params: List[Sym]
+    expr: Expr
+
+    def __call__(self, *args):
+        return FunCall(name=SymRef(id=str(self.id)), args=[*args])
 
 
 class Setq(Node):
@@ -67,6 +84,19 @@ class FencilDefinition(Node, SymbolTableTrait):
     id: SymbolName
     params: List[Sym]
     closures: List[StencilClosure]
+
+    def __call__(self, *args: Any, **kwargs: Any) -> Any:
+        if not len(args) == len(self.params):
+            raise RuntimeError("Incorrect number of arguments")
+
+        prog = Program(
+            function_definitions=_fundefs, fencil_definitions=[self], setqs=[]
+        )
+        if "backend" in kwargs:
+            if kwargs["backend"] == "lisp":
+                print(ToLispLike.apply(prog))
+            elif kwargs["backend"] == "c++":
+                print(ToyCpp.apply(prog))
 
 
 class Program(Node, SymbolTableTrait):
@@ -178,104 +208,175 @@ class ToyCpp(TemplatedGenerator):
         return formatted_code
 
 
-def s(id):
+def _s(id):
     return SymRef(id=id)
 
 
-def f(fun, *args):
+def _f(fun, *args):
     if isinstance(fun, str):
-        fun = s(fun)
+        fun = _s(fun)
     return FunCall(name=fun, args=[*args])
 
 
 def deref(arg):
-    return f("deref", arg)
+    return _f("deref", arg)
 
 
-def minus(arg0, arg1):
-    return f("minus", arg0, arg1)
+def shift(*offsets):
+    offsets = tuple(
+        OffsetLiteral(value=o) if isinstance(o, str) or isinstance(o, int) else o
+        for o in offsets
+    )
+    return _f("shift", *offsets)
 
 
-def shift(it, *offsets):
-    return f(f("shift", *offsets), it)
+def lift(sten):
+    return _f("lift", sten)
 
 
-ldif = FunctionDefinition(
-    id="ldif",
-    params=[Sym(id="d")],
-    expr=Lambda(
-        params=[Sym(id="in")],
-        expr=(
-            minus(
-                deref(
-                    shift(SymRef(id="in"), SymRef(id="d"), OffsetLiteral(value=-1))
-                ),  # What's OffsetLiteral?
-                deref(SymRef(id="in")),
-            )
-        ),
-    ),
-)
+def make_node(o):
+    if isinstance(o, Node):
+        return o
+    if callable(o):
+        if o.__name__ == "<lambda>":
+            return lambdadef(o)
+        if o.__code__.co_flags & inspect.CO_NESTED:
+            return lambdadef(o)
+    if isinstance(o, int):
+        return IntLiteral(value=o)
+    raise NotImplementedError(f"Cannot handle {o}")
 
-rdif = FunctionDefinition(
-    id="rdif",
-    params=[Sym(id="d")],
-    expr=f("compose", f("ldif", s("d")), f("shift", s("d"), OffsetLiteral(value=1))),
-)
 
-dif2 = FunctionDefinition(
-    id="dif2",
-    params=[Sym(id="d")],
-    expr=f("compose", f("ldif", s("d")), f("lift", f("rdif", s("d")))),
-)
-
-lap = FunctionDefinition(
-    id="lap",
-    params=[Sym(id="in")],
-    expr=f(
-        "plus",
-        f(f("dif2", OffsetLiteral(value="i")), s("in")),
-        f(f("dif2", OffsetLiteral(value="j")), s("in")),
-    ),
-)
-
-my_fencil = FencilDefinition(
-    id="testee",
-    params=[
-        Sym(id="xs"),
-        Sym(id="xe"),
-        Sym(id="ys"),
-        Sym(id="ye"),
-        Sym(id="z"),
-        Sym(id="output"),
-        Sym(id="input"),
-    ],
-    closures=[
-        StencilClosure(
-            domain=FunCall(
-                name=SymRef(id="cartesian"),
-                args=[
-                    SymRef(id="xs"),
-                    SymRef(id="xe"),
-                    SymRef(id="ys"),
-                    SymRef(id="ye"),
-                    IntLiteral(value=0),
-                    SymRef(id="z"),
-                ],
+def fundef(fun, *, is_lambda=False):
+    body = fun(*list(_s(param) for param in inspect.signature(fun).parameters.keys()))
+    body = make_node(body)
+    if is_lambda:
+        return Lambda(
+            params=list(
+                Sym(id=param) for param in inspect.signature(fun).parameters.keys()
             ),
-            stencil=SymRef(id="lap"),
-            outputs=[SymRef(id="output")],
-            inputs=[SymRef(id="input")],
+            expr=body,
         )
-    ],
-)
+    else:
+        res = FunctionDefinition(
+            id=fun.__name__,
+            params=list(
+                Sym(id=param) for param in inspect.signature(fun).parameters.keys()
+            ),
+            expr=body,
+        )
+        _fundefs.append(res)
+        return res
 
-prog = Program(
-    function_definitions=[ldif, rdif, dif2, lap],
-    fencil_definitions=[my_fencil],
-    setqs=[],
-)
-debug(prog)
 
-print(ToLispLike.apply(prog))
+def lambdadef(fun):
+    return fundef(fun, is_lambda=True)
 
-print(ToyCpp.apply(prog))
+
+def compose(*args):
+    return _f("compose", *args)
+
+
+def cartesian(*args):
+    return _f("cartesian", *args)
+
+
+def apply_stencil(domain, stencil, outputs, inputs):
+    return StencilClosure(
+        domain=domain,
+        stencil=SymRef(id=str(stencil.id)),
+        outputs=outputs,
+        inputs=inputs,
+    )
+
+
+def closure(*args):
+    return list(arg for arg in args)
+
+
+def fendef(fun):
+    res = fun(*list(_s(param) for param in inspect.signature(fun).parameters.keys()))
+
+    return FencilDefinition(
+        id=fun.__name__,
+        params=list(
+            Sym(id=param) for param in inspect.signature(fun).parameters.keys()
+        ),
+        closures=res,
+    )
+
+
+# user code ===============
+@fundef
+def ldif(d):
+    return lambda inp: deref(shift(d, -1)(inp)) - deref(inp)
+
+
+@fundef
+def rdif(d):
+    return compose(ldif(d), shift(d, 1))
+
+
+@fundef
+def dif2(d):
+    return compose(ldif(d), lift(rdif(d)))
+
+
+i = OffsetLiteral(value="i")
+j = OffsetLiteral(value="j")
+
+
+@fundef
+def lap(inp):
+    return dif2(i)(inp) + dif2(j)(inp)
+
+
+@fendef
+def testee(xs, xe, ys, ye, z, output, input):
+    return closure(apply_stencil(cartesian(xs, xe, ys, ye, z), lap, [output], [input]))
+
+
+# my_fencil = FencilDefinition(
+#     id="testee",
+#     params=[
+#         Sym(id="xs"),
+#         Sym(id="xe"),
+#         Sym(id="ys"),
+#         Sym(id="ye"),
+#         Sym(id="z"),
+#         Sym(id="output"),
+#         Sym(id="input"),
+#     ],
+#     closures=[
+#         StencilClosure(
+#             domain=FunCall(
+#                 name=SymRef(id="cartesian"),
+#                 args=[
+#                     SymRef(id="xs"),
+#                     SymRef(id="xe"),
+#                     SymRef(id="ys"),
+#                     SymRef(id="ye"),
+#                     IntLiteral(value=0),
+#                     SymRef(id="z"),
+#                 ],
+#             ),
+#             stencil=SymRef(id="lap"),
+#             outputs=[SymRef(id="output")],
+#             inputs=[SymRef(id="input")],
+#         )
+#     ],
+# )
+
+# prog = Program(
+#     function_definitions=[ldif, rdif, dif2, lap],
+#     fencil_definitions=[testee],
+#     setqs=[],
+# )
+# debug(prog)
+
+# print(ToLispLike.apply(prog))
+
+testee(*([None] * 7), backend="lisp")
+testee(*([None] * 7), backend="c++")
+
+# print(ToyCpp.apply(prog))
